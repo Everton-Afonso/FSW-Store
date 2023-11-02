7f021e2743583a2a0f9bd79e7b3c4ce3ccea73d7
Everton Afonso <evertonafonso0@gmail.com>
2023-11-02 17:48:04 -0300

    feat: add Catelog page

---
+++ src/app/Catalog/page.tsx
@@ -0,0 +1,28 @@
+import { Badge } from "@/components/ui/badge";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/categoryItem";
+import { prismaClient } from "@/lib/prisma";
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany({});
+
+  return (
+    <section className="flex flex-col gap-8 p-5">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant={"outline"}
+      >
+        <ShapesIcon size={16} />
+        Catálog
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8 lg:grid-cols-3">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CatalogPage;
