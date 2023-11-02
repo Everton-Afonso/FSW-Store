c87914821947624133067944b1dfea630bee8d09
Everton Afonso <evertonafonso0@gmail.com>
2023-11-02 17:02:06 -0300

    style: ensure footer is always on the bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,11 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <Header />
-          {children}
-          <Footer />
-        </AuthProvider>
+        <section className="flex h-full flex-col">
+          <AuthProvider>
+            <Header />
+            <div className="flex-1">{children}</div>
+            <Footer />
+          </AuthProvider>
+        </section>
       </body>
     </html>
   );
