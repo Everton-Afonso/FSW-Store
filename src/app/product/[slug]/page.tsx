4040cafea14207d3d98dd7dd4376dca42bbe3618
Everton Afonso <evertonafonso0@gmail.com>
2023-11-07 22:35:28 -0300

    feat: add ProductInfo component

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { CATEGORY_ICON } from "@/constants/category-icon";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
+import ProductInfo from "./components/product-info";
 
 interface ProductDetailsPageProps {
   params: {
@@ -23,8 +24,9 @@ const ProductDetailsPage = async ({
   if (!product) return null;
 
   return (
-    <section>
+    <section className="flex flex-col gap-8">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
+      <ProductInfo product={computeProductTotalPrice(product)} />
     </section>
   );
 };
