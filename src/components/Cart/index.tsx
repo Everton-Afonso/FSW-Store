856fd4e7c58037d3c7b3ff854188c9d9e6221956
Everton Afonso <evertonafonso0@gmail.com>
2023-11-18 18:00:31 -0300

    feat: add CartItem component

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -4,11 +4,13 @@ import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "../ui/badge";
 
 import { CartContext } from "@/providers/cart";
+import CartItem from "../CartItem";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 const Cart = () => {
   const { products } = useContext(CartContext);
   return (
-    <section>
+    <section className="flex flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -17,9 +19,14 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      {products.map((product) => (
-        <h1 key={product.id}>{product.name}</h1>
-      ))}
+      <div className="flex flex-col gap-5">
+        {products.map((product) => (
+          <CartItem
+            key={product.id}
+            product={computeProductTotalPrice(product as any) as any}
+          />
+        ))}
+      </div>
     </section>
   );
 };
